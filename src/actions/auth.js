d47263ae1fa5001b6392dc46111940eabae915ae
DESKTOP-SD5KS9S\SANDWOOD <sandwood09@gmail.com>
2017-10-31 13:02:00 +0900

    Login/Logout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,4 @@
-import { USER_LOGGED_IN } from '../types';
+import { USER_LOGGED_IN,USER_LOGGED_OUT } from '../types';
 import api from '../api';
 
 export const userLoggedIn = user => ({
@@ -6,5 +6,17 @@ export const userLoggedIn = user => ({
     user
 })
 
+export const userLoggedOut = () => ({
+    type: USER_LOGGED_OUT
+})
+
 export const login = (credentials) => (dispath) => 
-    api.user.login(credentials).then(user => dispath(userLoggedIn(user)));
+    api.user.login(credentials).then(user => {
+        localStorage.bookwormJWT = user.token;
+        dispath(userLoggedIn(user))
+    });
+    
+export const logout = () => dispath => {
+    localStorage.removeItem('bookwormJWT');
+    dispath(userLoggedOut());
+};
--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import rootReducer from './rootReducer';
+import { userLoggedIn } from './actions/auth';
 
 const store = createStore(
     rootReducer, 
     composeWithDevTools(applyMiddleware(thunk))
 );
+
+if (localStorage.bookwormJWT) {
+    const user = { token: localStorage.bookwormJWT };
+    store.dispatch(userLoggedIn(user));
+}
 
 ReactDOM.render(
     <BrowserRouter>
