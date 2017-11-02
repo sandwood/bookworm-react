783075d3e7dc9ba1cc94ac9b9b302935b0fba6fd
DESKTOP-SD5KS9S\SANDWOOD <sandwood09@gmail.com>
2017-11-02 12:17:06 +0900

    reset password

--- src/api.js
+++ src/api.js
@@ -13,6 +13,15 @@ export default {
     confirm: token =>
       axios
         .post("http://localhost:8080/api/auth/confirmation", { token })
-        .then(res => res.data.user)
+        .then(res => res.data.user),
+    resetPasswordRequest: email =>
+      axios
+        .post("http://localhost:8080/api/auth/reset_password_request", { email }),
+    validateToken: token => 
+      axios
+        .post("http://localhost:8080/api/auth/validate_token", { token }),
+    resetPassword: data => 
+      axios
+        .post('http://localhost:8080/api/auth/reset_password', { data })
   }
 };
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import LoginForm from '../forms/LoginForm';
-import { login } from '../../actions/auth';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import LoginForm from "../forms/LoginForm";
+import { login } from "../../actions/auth";
 
 class LoginPage extends Component {
-    submit = data =>     
-        this.props.login(data).then(() => this.props.history.push('/dashboard'));
-        
-    render() {
-        return (
-            <div>
-                <h1>Login Page</h1>
+  submit = data =>
+    this.props.login(data).then(() => this.props.history.push("/dashboard"));
 
-                <LoginForm submit={this.submit} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+
+        <LoginForm submit={this.submit} />
+
+        <Link to="/forgot_password">Forgot Password?</Link>
+      </div>
+    );
+  }
 }
 
 LoginPage.propTypes = {
-    history: PropTypes.shape({
-        push: PropTypes.func.isRequired
-    }).isRequired,
-    login: PropTypes.func.isRequired
-}
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
 
 export default connect(null, { login })(LoginPage);
