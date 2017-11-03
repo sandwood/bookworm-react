ef51471dd4f4fb83c16c1148400a0dc49ae10dfc
DESKTOP-SD5KS9S\SANDWOOD <sandwood09@gmail.com>
2017-11-03 15:33:54 +0900

    goodbooks xml convert to js and etc

--- src/components/forms/SearchBookForm.js
+++ src/components/forms/SearchBookForm.js
@@ -1,14 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import axios from "axios";
 import { Form, Dropdown } from "semantic-ui-react";
 
-class SeacrhBookFrom extends Component {
+class SearchBookForm extends React.Component {
   state = {
     query: "",
     loading: false,
     options: [],
     books: {}
   };
+
   onSearchChange = (e, data) => {
     clearTimeout(this.timer);
     this.setState({
@@ -17,12 +19,30 @@ class SeacrhBookFrom extends Component {
     this.timer = setTimeout(this.fetchOptions, 1000);
   };
 
+  onChange = (e, data) => {
+    this.setState({ query: data.value });
+    this.props.onBookSelect(this.state.books[data.value]);
+  };
+
   fetchOptions = () => {
     if (!this.state.query) return;
     this.setState({ loading: true });
     axios
-      .get(`/api/books/search?q=${this.state.query}`)
-      .then(res => res.data.books);
+      .get(`/api/books/search?q=${this.state.query.searchQuery}`)
+      .then(res => res.data.books)
+      .then(books => {
+        const options = [];
+        const booksHash = {};
+        books.forEach(book => {
+          booksHash[book.goodreadsId] = book;
+          options.push({
+            key: book.goodreadsId,
+            value: book.goodreadsId,
+            text: book.title
+          });
+        });
+        this.setState({ loading: false, options, books: booksHash });
+      });
   };
 
   render() {
@@ -36,10 +56,15 @@ class SeacrhBookFrom extends Component {
           onSearchChange={this.onSearchChange}
           options={this.state.options}
           loading={this.state.loading}
+          onChange={this.onChange}
         />
       </Form>
     );
   }
 }
 
-export default SeacrhBookFrom;
+SearchBookForm.propTypes = {
+  onBookSelect: PropTypes.func.isRequired
+};
+
+export default SearchBookForm;
